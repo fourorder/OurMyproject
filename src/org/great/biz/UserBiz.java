1a1bfc9edd1006f78b4d48857556f0e085d359cf
vv871870674 <vv23884327>
2018-09-20 09:40:08 +0800

    newupdate

--- src/org/great/biz/UserBiz.java
+++ src/org/great/biz/UserBiz.java
@@ -3,5 +3,5 @@ package org.great.biz;
 import java.util.List;
 
 public interface UserBiz {
-	public List<Object> search(String name);
+	public List<Object> search(String name,String page,String state);
 }
